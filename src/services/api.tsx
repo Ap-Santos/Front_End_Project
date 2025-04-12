345ed7a370f4b5418eb37512b5b6147211ad61f9
Ariel Santos <arielpereirasantos104@gmail.com>
2025-04-12 20:24:01 -0300

    feat: listar produtos com carregamento e tabela

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,8 +1,8 @@
-import axios from 'axios'
+import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: 'https://fakestoreapi.com/products'
+    baseURL: "https://fakestoreapi.com",
 });
 
 
