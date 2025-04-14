f5ff9d3ba240db3a71d42be0a787c1b48094f544
Ariel Santos <arielpereirasantos104@gmail.com>
2025-04-14 16:33:05 -0300

    Criação e formatação da tabela

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,7 +1,8 @@
 import { TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import { useProducts } from "../hooks/useProducts";
 import Table from '@mui/material/Table';
-import React from "react";
+import Paper from '@mui/material/Paper';
+import { Container } from "@mui/material";
 const ProductsPage = () => {
     const { products, loading } = useProducts();
 
@@ -9,32 +10,42 @@ const ProductsPage = () => {
         return <p>Carregando produtos...</p>
     }
     return (
-        <div>
-            <h1>Lista de Produtos</h1>
-            <TableContainer>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Nome</TableCell>
-                            <TableCell>Preço</TableCell>
-                            <TableCell>Categoria</TableCell>
-                            
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                    { products.map((product)=> (
-                        <TableRow key ={product.id}>
-                            <TableCell>{product.title}</TableCell>
-                            <TableCell>{product.price}</TableCell>
-                            <TableCell>{product.category}</TableCell>
-                        </TableRow>
-                        ))}
-                    </TableBody>
-                   
-                </Table>
-            </TableContainer>
-        </div>
+        <Container>
+            <div>
+                <h1>Lista de Produtos</h1>
+                <Paper sx={{ p: 2, mt: 4, mb: 4 }} elevation={3}>
+                    <TableContainer sx={{ maxHeight: 600 }}>
+                        <Table stickyHeader>
+                            <TableHead >
+                                <TableRow>
+                                <TableCell sx={{ backgroundColor: '#f5f5f5', fontWeight: 'bold' }}>ID</TableCell>
+                                    <TableCell sx={{ backgroundColor: '#f5f5f5', fontWeight: 'bold' }}>Nome</TableCell>
+                                    <TableCell sx={{ backgroundColor: '#f5f5f5', fontWeight: 'bold' }}>Preço</TableCell>
+                                    <TableCell sx={{ backgroundColor: '#f5f5f5', fontWeight: 'bold' }}>Categoria</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {products.map((product) => (
+                                    <TableRow key={product.id}
+                                        hover
+                                        sx={{ cursor: "pointer" }}>
+                                            <TableCell>{product.id}</TableCell>
+                                        <TableCell align="left">{product.title}</TableCell>
+                                        <TableCell align="right">{product.price}</TableCell>
+                                        <TableCell>{product.category}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Paper>
+            </div>
+
+            <div>
+                <h1>Formulario</h1>
+            </div>
+        </Container>
+
     );
 };
-
 export default ProductsPage;
